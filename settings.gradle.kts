3bb12d2c90ab3a74d5c448e46d74f30b329baf56
Rubinder Randhawa <robsrandhawa@gmail.com>
2026-05-04 21:48:02 -0400

    Phase 3 task 1: version catalog + Confluent maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,15 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
+        maven {
+            name = "Confluent"
+            url = uri("https://packages.confluent.io/maven/")
+            content {
+                includeGroup("io.confluent")
+                // The Avro confluent registry connector is also published here.
+                includeGroup("org.apache.flink")
+            }
+        }
     }
 }
 
